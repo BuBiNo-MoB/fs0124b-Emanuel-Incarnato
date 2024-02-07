e47490f7b14124bf170f45c8871c23598e33d375
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-02-07 16:56:17 +0100

    Consegna S3/L3

---
+++ M1/W3/G3/assets/D3.js
@@ -0,0 +1,10 @@
+const bottone = document.querySelector("#bottone");
+
+const testo = document.querySelector('#task');
+
+const elenco = document.createElement("div");
+
+bottone.addEventListener('click', () => 
+    document.body.appendChild(document.createElement("div")))
+
+console.log(elenco)
