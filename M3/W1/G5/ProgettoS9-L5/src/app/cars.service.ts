56f9389a30319961c96ee37d9f18ace3a72d64b3
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-03-22 14:27:15 +0100

    Consegna S9/L5

--- M3/W1/G5/ProgettoS9-L5/src/app/cars.service.ts
+++ M3/W1/G5/ProgettoS9-L5/src/app/cars.service.ts
@@ -6,18 +6,4 @@ import { iCars } from './models/cars';
 })
 export class CarsService {
 
-  getAllCars():Promise<iCars[]>{
-    return fetch ('../assets/db.json')
-    .then(car => car.json())
-    .then((cars:iCars[]) => cars)
-  }
-  getActiveCars():Promise<iCars[]>{
-    return this.getAllCars()
-    .then(article => article.filter(p => p.available))
-  }
-
-  getInactiveCars():Promise<iCars[]>{
-    return this.getAllCars()
-    .then(article => article.filter(p => p.available))
-  }
 }
