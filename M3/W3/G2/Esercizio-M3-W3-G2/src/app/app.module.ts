b44b4dc51a0db7155f3a7bada7219a4cca69162f
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-04-03 15:14:07 +0200

    Consegna S11/L2

---
+++ M3/W3/G2/Esercizio-M3-W3-G2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './maincomponents/header/header.component';
+import { FooterComponent } from './maincomponents/footer/footer.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
