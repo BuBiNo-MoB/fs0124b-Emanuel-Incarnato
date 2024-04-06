4500a076d928774d1723558721253ea7540840e0
BuBiNo-MoB <emanuel.incarnato@gmail.com>
2024-04-06 11:58:40 +0200

    test

--- M3/W3/G4/Progetto-M3-W3/src/app/auth/register/register.component.ts
+++ M3/W3/G4/Progetto-M3-W3/src/app/auth/register/register.component.ts
@@ -1,4 +1,8 @@
+import { AuthService } from './../auth.service';
 import { Component } from '@angular/core';
+import { ILoginData } from '../../modules/i-login-data';
+import { IUser } from '../../modules/i-user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  registerData:Partial<IUser> = {}
+
+  constructor(
+    private authSvc:AuthService,
+    private router:Router
+    ){}
+
+  signUp(){
+    this.authSvc.register(this.registerData)
+    .subscribe(data => {
+
+      this.router.navigate(['dashboard'])
+
+    })
+  }
 
 }
---
+++ M3/W3/G4/Progetto-M3-W3/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  registerUrl : 'http://localhost:4200/register',
+
+  loginUrl : 'http://localhost:4200/login'
+};
